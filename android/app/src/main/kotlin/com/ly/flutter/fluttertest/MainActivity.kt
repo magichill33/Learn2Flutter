b5007646613de647395e4a1a84aef26a46a5d29b
liuyi <liuyi12@kingsoft.com>
2023-02-27 20:23:02 +0800

    flutter小示例程序

---
+++ android/app/src/main/kotlin/com/ly/flutter/fluttertest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ly.flutter.fluttertest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(home: Scaffold(
+//       appBar: AppBar(title: const Text("First Flutter")),
+//       body: _MyStateless())));
+// }
+//
+// class _MyStateless extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Center(child: Text("Hello World"));
+//   }
+//
+// }
